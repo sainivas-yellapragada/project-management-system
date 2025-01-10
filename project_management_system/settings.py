c89c352b7138bde042ff3ac37d812352a760bec8
sainivas-yellapragada <sainivasyellapragada@gmail.com>
2025-01-10 10:22:24 +0530

    added db data in settings.py

--- project_management_system/settings.py
+++ project_management_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d#_xi#s!0$3$pi%v(79j!nl9cf0qlx-c-w3s&@&8c#)!^4jhj5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pms',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
